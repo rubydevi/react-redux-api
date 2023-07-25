057894b769807c9ed608f4b2a091139a323ee1d9
rubydevi <ariesabyss@gmail.com>
2023-07-25 14:42:29 +0530

    Fetch API and create extraReducers

--- src/redux/users/usersSlice.js
+++ src/redux/users/usersSlice.js
@@ -11,9 +11,7 @@ export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
   try {
     // Fetch API
     const response = await fetch('https://randomuser.me/api/?results=5');
-    const data = response.json();
-    console.log(data.results);
-    console.log(data.results.name);
+    const data = await response.json();
     return data.results;
   } catch (error) {
     throw Error('Failed to fetch users');
@@ -24,7 +22,21 @@ const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.isLoading = true;
+        state.error = undefined;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default usersSlice.reducer;
