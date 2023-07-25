6d0c9bf0ea82701622f05b4f0e1fd48bef094632
rubydevi <ariesabyss@gmail.com>
2023-07-25 14:43:18 +0530

    Import the user list in the App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchUsers } from './redux/users/usersSlice'
+import UserList from './components/UserList';
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="App">
+      <h1>Random Users</h1>
+      <UserList />
     </div>
-  )
+  );
 }
 
-export default App;
+export default App
