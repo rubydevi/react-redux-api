652b58d111889eb97a50f650ea1e6394d09c948d
rubydevi <ariesabyss@gmail.com>
2023-07-25 14:12:45 +0530

    Create action creator to fetch API

--- src/redux/users/usersSlice.js
+++ src/redux/users/usersSlice.js
@@ -1,10 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
   users: [],
   isLoading: false,
   error: undefined,
 };
+
+// Action creator that fetches users
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  try {
+    // Fetch API
+    const response = await fetch('https://randomuser.me/api/?results=5');
+    const data = response.json();
+    console.log(data.results);
+    console.log(data.results.name);
+    return data.results;
+  } catch (error) {
+    throw Error('Failed to fetch users');
+  }
+});
 
 const usersSlice = createSlice({
   name: 'users',
